5344bea67a225df24d195e0fcfe0e1bdf6605e95
Jarod Whiting <whitingjarod@protonmail.com>
2024-01-09 08:07:08 -0700

    Hard-coded shift+leftclick to resize floating windows.

--- leftwm-core/src/handlers/mouse_combo_handler.rs
+++ leftwm-core/src/handlers/mouse_combo_handler.rs
@@ -52,14 +52,27 @@ impl State {
         modifier: ModMask,
     ) -> Option<DisplayAction> {
         let is_mouse_key = mod_mask == modifier || mod_mask == (modifier | xlib::ShiftMask);
+        let is_shift_key = mod_mask == (modifier | xlib::ShiftMask);
         match button {
             xlib::Button1 if is_mouse_key => {
-                _ = self
-                    .windows
-                    .iter()
-                    .find(|w| w.handle == window && w.can_move())?;
-                self.mode = Mode::ReadyToMove(window);
-                Some(DisplayAction::ReadyToMoveWindow(window))
+                match is_shift_key {
+                    true => {
+                        _ = self
+                            .windows
+                            .iter()
+                            .find(|w| w.handle == window && w.can_resize())?;
+                        self.mode = Mode::ReadyToResize(window);
+                        Some(DisplayAction::ReadyToResizeWindow(window))
+                    }
+                    false => {
+                        _ = self
+                            .windows
+                            .iter()
+                            .find(|w| w.handle == window && w.can_move())?;
+                        self.mode = Mode::ReadyToMove(window);
+                        Some(DisplayAction::ReadyToMoveWindow(window))
+                    }
+                }
             }
             xlib::Button3 if is_mouse_key => {
                 _ = self
